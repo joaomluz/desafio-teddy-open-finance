fa98b9698adde77bbd57c433bcbef2edd43d981e
Joao <joaomluz@gmail.com>
2025-11-10 20:38:23 -0300

    add api.alertapx.com to CORS

--- apps/back-end/src/main.ts
+++ apps/back-end/src/main.ts
@@ -23,6 +23,8 @@ async function bootstrap() {
       origin: [
         'http://localhost:5173',
         'http://127.0.0.1:5173',
+        'https://api.alertapx.com',
+        'http://api.alertapx.com',
         process.env.FRONTEND_URL || 'http://localhost:5173'
       ],
       credentials: true,
